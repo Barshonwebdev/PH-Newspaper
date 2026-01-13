766a1d1cd94588f5c7c5a44b07fc101201b380cd
Barshon <barshonweb@gmail.com>
2026-01-13 21:48:07 +0600

    navbar create

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,30 @@
 export default function Home() {
   return (
     <div className="">
-     
+      <div className="navbar bg-indigo-50 shadow-sm">
+        <div className="flex-1">
+          <a className="btn btn-ghost text-3xl ">PH News</a>
+        </div>
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1 text-xl">
+            <li>
+              <a>Politics</a>
+            </li>
+            <li>
+              <a>Sports</a>
+            </li>
+            <li>
+              <a>Economics</a>
+            </li>
+            <li>
+              <a>Cultural</a>
+            </li>
+            <li>
+              <a>Wildlife</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
