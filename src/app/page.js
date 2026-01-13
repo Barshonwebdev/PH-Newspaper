909e20222b0ec3da9278a2b90aff462015d1848c
Barshon <barshonweb@gmail.com>
2026-01-14 00:09:13 +0600

    redirect homepage to news page

--- src/app/page.js
+++ src/app/page.js
@@ -1,30 +1,5 @@
-export default function Home() {
-  return (
-    <div className="">
-      <div className="navbar bg-indigo-50 shadow-sm">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-3xl ">PH News</a>
-        </div>
-        <div className="flex-none">
-          <ul className="menu menu-horizontal px-1 text-xl">
-            <li>
-              <a>Politics</a>
-            </li>
-            <li>
-              <a>Sports</a>
-            </li>
-            <li>
-              <a>Economics</a>
-            </li>
-            <li>
-              <a>Cultural</a>
-            </li>
-            <li>
-              <a>Wildlife</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-  );
+import { redirect } from "next/navigation";
+
+export default function HomePage() {
+  redirect("/news");
 }
